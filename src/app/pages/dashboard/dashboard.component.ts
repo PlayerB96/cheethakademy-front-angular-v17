ccaf3d1f6e60fb90bee22eb58ab279f40751c83e
Playerb96 <player.b.96@hotmail.com>
2024-03-13 10:01:08 -0500

    test netlify

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CommonModule } from '@angular/common';
+
 export interface Tile {
   color: string;
   cols: number;
@@ -9,15 +15,30 @@ export interface Tile {
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, CommonModule, FlexLayoutModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
   tiles: Tile[] = [
-    { text: 'One', cols: 3, rows: 1, color: '#1F2937' },
+    { text: 'One', cols: 2, rows: 1, color: '#1F2937' },
     { text: 'Two', cols: 1, rows: 2, color: '#1F2937' },
     { text: 'Three', cols: 1, rows: 1, color: '#1F2937' },
     { text: 'Four', cols: 2, rows: 1, color: '#1F2937' },
   ];
+
+  cols$: Observable<number>;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.cols$ = this.breakpointObserver
+      .observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .pipe(
+        map(({ matches }) => {
+          if (matches) {
+            return 2;
+          }
+          return 4;
+        })
+      );
+  }
 }
