1ff377ffa1991d75f4e584f6f09a95ecfcb99113
Playerb96 <player.b.96@hotmail.com>
2024-03-17 12:40:58 -0500

    move netlify.toml in src

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,4 @@
-const apiUrl = 'https://143.244.144.235:8000';
+const apiUrl = 'http://143.244.144.235:8000';
 export const constants = {
   CURRENT_TOKEN: 'CURRENT_TOKEN',
   DATA_USER: 'DATA_USER',
